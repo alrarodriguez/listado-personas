96cb52ef5f02210702f57272c149b471b0410441
Alvaro Atunez <alvaroatunez@gmail.com>
2021-12-24 05:41:26 -0500

    Actualización

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { People } from './people.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de personas';
+  peoples: People[] = [];
+
+  fullNameInput: string = '';
+  identificationInput: number = 0;
+  birthInput: string = '';
+  constructor(private http: HttpClient) {
+    this.http.get<People[]>('http://localhost:3000/people').subscribe((val: any)=> {
+      this.peoples = val;
+    });
+  }
+
+  addPeople(){
+    const people1: People = {
+      "fullName":this.fullNameInput,
+      "identification": this.identificationInput,
+      "birth": this.birthInput
+  }; 
+    let validate = false;
+    this.peoples.filter((item) => {
+      if(item.identification === this.identificationInput) {
+        validate = true
+      };
+  });
+    if ( !validate ) {
+      this.peoples.push(people1);
+      console.log('Client saved: ',people1);
+      
+    this.http.post('http://localhost:3000/people', people1).subscribe((val) => {
+      console.log('Response ',val);
+    });
+    }
+    this.fullNameInput = '';
+    this.birthInput = '';
+    this.identificationInput = 0;
+
+  }
+
+  deletePeople(identification: number) {
+      this.peoples = this.peoples.filter(function(item) {
+        return item.identification !== identification
+    })
+    this.http.delete('http://localhost:3000/people/'+identification).subscribe((val) => {
+      console.log('Response ',val);
+    });
+  }
+
 }
---
+++ src/app/people.model.ts
@@ -0,0 +1,13 @@
+export class People{
+    fullName: string ;
+    birth: string;
+    identification: number;
+
+
+    constructor(){
+        this.fullName = "";
+        this.birth = "inBirth";
+        this.identification = 0;
+    }
+
+}
